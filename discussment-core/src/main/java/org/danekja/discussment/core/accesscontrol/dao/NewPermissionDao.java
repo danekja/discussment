f4fab9b7b06210f7c34c9e29677a98e72b3a8be0
Jakub Daněk <jakub.danek@yoso.fi>
2017-11-19 20:10:47 +0100

    [re #20] new permission DAO interface

---
+++ discussment-core/src/main/java/org/danekja/discussment/core/accesscontrol/dao/NewPermissionDao.java
@@ -0,0 +1,13 @@
+package org.danekja.discussment.core.accesscontrol.dao;
+
+import org.danekja.discussment.core.accesscontrol.domain.AbstractPermission;
+import org.danekja.discussment.core.accesscontrol.domain.PermissionId;
+import org.danekja.discussment.core.dao.GenericDao;
+
+/**
+ * TODO rename this to {@link PermissionDao} before release
+ */
+public interface NewPermissionDao extends GenericDao<PermissionId, AbstractPermission> {
+
+
+}
