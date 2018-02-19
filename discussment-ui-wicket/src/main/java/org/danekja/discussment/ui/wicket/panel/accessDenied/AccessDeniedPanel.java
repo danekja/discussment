9ba8524f8c1a6ad4358a74202b5461c1f2f62c53
Jiri Kryda <jiri.kryda@yoso.fi>
2018-02-19 16:44:59 +0100

    [#re 20] Added access denied panel.

---
+++ discussment-ui-wicket/src/main/java/org/danekja/discussment/ui/wicket/panel/accessDenied/AccessDeniedPanel.java
@@ -0,0 +1,7 @@
+package org.danekja.discussment.ui.wicket.panel.accessDenied;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class AccessDeniedPanel extends Panel {
+    public AccessDeniedPanel(String id) {super(id);}
+}
