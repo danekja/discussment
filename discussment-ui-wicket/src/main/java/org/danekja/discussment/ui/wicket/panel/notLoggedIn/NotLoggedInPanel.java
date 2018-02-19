8a7b8c0d630a9bbc846076acacd830fa7d3695a6
Jiri Kryda <jiri.kryda@yoso.fi>
2018-02-19 16:45:45 +0100

    [#re 20] Added user not logged in panel.

---
+++ discussment-ui-wicket/src/main/java/org/danekja/discussment/ui/wicket/panel/notLoggedIn/NotLoggedInPanel.java
@@ -0,0 +1,7 @@
+package org.danekja.discussment.ui.wicket.panel.notLoggedIn;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class NotLoggedInPanel extends Panel {
+    public NotLoggedInPanel (String id) { super(id);}
+}
