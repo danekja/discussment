bff123db40d6bca585ea0f98aca486161e1c70c9
Jiri Kryda <jiri.kryda@yoso.fi>
2018-03-15 16:39:45 +0100

    [re #24] Removed unnecessary query

--- discussment-core/src/main/java/org/danekja/discussment/core/dao/jpa/TopicDaoJPA.java
+++ discussment-core/src/main/java/org/danekja/discussment/core/dao/jpa/TopicDaoJPA.java
@@ -22,9 +22,4 @@ public class TopicDaoJPA extends GenericDaoJPA<Long, Topic> implements TopicDao 
         q.setParameter("categoryId", category.getId());
         return q.getResultList();
     }
-
-    public List<Topic> getTopicsWithoutCategory() {
-        TypedQuery<Topic> q = em.createNamedQuery(Topic.GET_TOPICS_WITHOUT_CATEGORY, Topic.class);
-        return q.getResultList();
-    }
 }
