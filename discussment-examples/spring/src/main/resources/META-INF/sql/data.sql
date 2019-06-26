f07d3be3d7151e521b8a8eeff3eada5f0b948d3c
Pavel Fidranský <fidransky@outlook.com>
2019-06-26 15:14:26 +0200

    Spring example uses Flyway for DB migrations

--- discussment-examples/spring/src/main/resources/META-INF/sql/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Category VALUES (0, "default category");
-
-INSERT INTO Topic(id, name, category_id) VALUES (0, "default topic", 1)
-
-INSERT INTO Discussion(id, name, topic_id) VALUES (0, "default discussion", 1)
