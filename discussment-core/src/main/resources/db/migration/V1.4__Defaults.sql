712da75f42dfd6c46e622197009936a23e7b8a86
Pavel Fidranský <fidransky@outlook.com>
2019-06-26 15:05:47 +0200

    Add default discussion to DB migration script

--- discussment-core/src/main/resources/db/migration/V1.4__Defaults.sql
+++ discussment-core/src/main/resources/db/migration/V1.4__Defaults.sql
@@ -1,2 +1,3 @@
 INSERT INTO `discussment_category` (id, name) VALUES (1, 'default category');
 INSERT INTO `discussment_topic` (id, name, category_id) VALUES (1, 'default topic', 1);
+INSERT INTO `discussment_discussion` (id, name, topic_id) VALUES (1, 'default discussion', 1);
